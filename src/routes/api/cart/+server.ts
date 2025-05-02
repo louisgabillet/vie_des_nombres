519bf133ea18a366ccaa9577c60d6070a5217b59
Louis Gabillet <gabillet.louis@gmail.com>
2025-05-02 21:34:53 +0200

    refactor: remove unused APIs for cart

--- src/routes/api/cart/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json, type RequestHandler } from "@sveltejs/kit";
-import { getCart } from "$lib/server/cart";
-import type { Cart } from "$lib/cart/types";
-
-export const GET: RequestHandler = ({ locals }) => {
-    const cart: Cart = getCart(locals.cartId);
-
-    return json(cart);
-}
--- src/routes/api/cart/clear/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json, type RequestHandler } from "@sveltejs/kit";
-import { clearCart } from "$lib/server/cart";
-import type { Cart } from "$lib/cart/types";
-
-export const DELETE: RequestHandler= async ({ locals  }) => {
-    const cart: Cart = clearCart(locals.cartId);
-
-    return json(cart);
-}
